1ae5c8313bc815da77f92c346ddf770995219d4e
Jack Gong <jackgong48@gmail.com>
2024-08-02 14:51:37 +1200

    CHANGED: store to another one

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 
 export const createClient = (publicAccessToken: string) => {
   return createStorefrontApiClient({
-    storeDomain: "http://lakeside-1.myshopify.com",
+    storeDomain: "http://lakeside-4.myshopify.com",
     apiVersion: "2023-10",
     publicAccessToken: publicAccessToken,
     customFetchApi: fetch,
