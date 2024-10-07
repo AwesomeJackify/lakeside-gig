ed48ca1c86484d137323eb735f80524526a839e4
Jack Gong <jackgong48@gmail.com>
2024-10-07 20:28:30 +1300

    fix: right items null

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -25,9 +25,7 @@ const Nav = ({ token }: Props) => {
     },
   ];
 
-  const rightItems = [
-
-  ];
+  const rightItems = [{}];
 
   const bodyElement = document.getElementsByTagName("body")[0];
 
@@ -43,8 +41,9 @@ const Nav = ({ token }: Props) => {
 
   return (
     <nav
-      className={`px-4 py-4 z-50 ${showMobileMenu ? "fixed w-full" : "sticky"
-        } top-0`}
+      className={`px-4 py-4 z-50 ${
+        showMobileMenu ? "fixed w-full" : "sticky"
+      } top-0`}
     >
       <div className="grid grid-cols-3 items-center w-full justify-between max-lg:hidden text-xs">
         <div className="flex items-center gap-8">
@@ -105,8 +104,9 @@ const Nav = ({ token }: Props) => {
         <div className="flex items-center gap-2">
           <Cart token={token} />
           <button
-            className={`border-2 p-1 rounded-md border-black ${showMobileMenu ? "bg-black text-white" : ""
-              }`}
+            className={`border-2 p-1 rounded-md border-black ${
+              showMobileMenu ? "bg-black text-white" : ""
+            }`}
             onClick={() => setShowMobileMenu(!showMobileMenu)}
           >
             MENU
@@ -114,8 +114,9 @@ const Nav = ({ token }: Props) => {
         </div>
       </div>
       <div
-        className={`fixed top-0 left-0 right-0 md:hidden bottom-0 bg-white ${showMobileMenu ? "" : "hidden"
-          }`}
+        className={`fixed top-0 left-0 right-0 md:hidden bottom-0 bg-white ${
+          showMobileMenu ? "" : "hidden"
+        }`}
       >
         <div className="flex flex-col gap-4 py-4 mt-16 ml-6">
           {leftItems.map((leftItem) => (
@@ -127,7 +128,7 @@ const Nav = ({ token }: Props) => {
               {leftItem.name}
             </a>
           ))}
-          {rightItems.map((rightItem) => (
+          {/* {rightItems.map((rightItem) => (
             <a
               href={rightItem.url}
               className="uppercase link link-hover"
@@ -135,7 +136,7 @@ const Nav = ({ token }: Props) => {
             >
               {rightItem.name}
             </a>
-          ))}
+          ))} */}
         </div>
       </div>
     </nav>
