0f7255ecb7e15374aded40cd072b788f0e1f21d9
Jack Gong <jackgong48@gmail.com>
2024-10-19 11:42:37 +1300

    UPDATED: contact header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,8 +20,8 @@ const Nav = ({ token }: Props) => {
       url: "/campaign",
     },
     {
-      name: "contact",
-      url: "/contact",
+      name: "info",
+      url: "/info",
     },
   ];
 
@@ -41,9 +41,8 @@ const Nav = ({ token }: Props) => {
 
   return (
     <nav
-      className={`px-4 py-4 z-50 ${
-        showMobileMenu ? "fixed w-full" : "sticky"
-      } top-0`}
+      className={`px-4 py-4 z-50 ${showMobileMenu ? "fixed w-full" : "sticky"
+        } top-0`}
     >
       <div className="grid grid-cols-3 items-center w-full justify-between max-lg:hidden text-xs">
         <div className="flex items-center gap-8">
@@ -104,9 +103,8 @@ const Nav = ({ token }: Props) => {
         <div className="flex items-center gap-2">
           <Cart token={token} />
           <button
-            className={`border-2 p-1 rounded-md border-black ${
-              showMobileMenu ? "bg-black text-white" : ""
-            }`}
+            className={`border-2 p-1 rounded-md border-black ${showMobileMenu ? "bg-black text-white" : ""
+              }`}
             onClick={() => setShowMobileMenu(!showMobileMenu)}
           >
             MENU
@@ -114,9 +112,8 @@ const Nav = ({ token }: Props) => {
         </div>
       </div>
       <div
-        className={`fixed top-0 left-0 right-0 md:hidden bottom-0 bg-white ${
-          showMobileMenu ? "" : "hidden"
-        }`}
+        className={`fixed top-0 left-0 right-0 md:hidden bottom-0 bg-white ${showMobileMenu ? "" : "hidden"
+          }`}
       >
         <div className="flex flex-col gap-4 py-4 mt-16 ml-6">
           {leftItems.map((leftItem) => (
