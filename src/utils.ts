a82e4c774f7d3c0730addf3b6b858bbe89cd4a14
Jack Gong <jackgong48@gmail.com>
2025-03-27 11:35:24 +1300

    UPDATED: storefront api version

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 export const createClient = (publicAccessToken: string) => {
   return createStorefrontApiClient({
     storeDomain: "http://lakeside-4.myshopify.com",
-    apiVersion: "2023-10",
+    apiVersion: "2024-04",
     publicAccessToken: publicAccessToken,
     customFetchApi: fetch,
   });
